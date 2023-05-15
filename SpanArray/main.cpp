57ce57f318c938160de6c8f845f869f6df4fac74
Eric WIstrand <eric.j.wistrand@gmail.com>
2023-05-14 22:02:50 -0700

    initial check-in

---
+++ SpanArray/main.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+
+#include <span>
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    std::span span{ array, 10 }; 
+    // Iterate over all elements of the span
+    for (auto element : span) {
+        cout << element << " ";
+    }
+    cout << std::endl; 
+    // Access a single element at a specified index   
+    cout << "Element at index 2: " << span[2] << std::endl; 
+    // Get the size of the span 
+    cout << "Size of span: " << span.size() << std::endl;
+    return 0;
+}
